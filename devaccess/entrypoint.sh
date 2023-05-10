858299573ed604b071ae43adf9ef0f165b8ce020
Francesco Romani <fromani@redhat.com>
2023-05-10 11:05:51 +0200

    devaccess: implement vfio devices access check
    Implement the script entrypoint to verify
    the container actually has VFIO devices allocated
    *and accessible*, crashing hard if not.
    
    Signed-off-by: Francesco Romani <fromani@redhat.com>

--- devaccess/entrypoint.sh
+++ devaccess/entrypoint.sh
@@ -1,7 +1,33 @@
 #!/bin/bash
-# 1. find all the <vfid>s allocated to the container
-# 2. check all /dev/vfio/<vfid> are accessible within the container (R_ACCESS)
-# 3. crash if not (exit 1)
-# 4. sleep forever otherwise
+# do we have a `/dev/vfio` directory accessible at all?
+if [ ! -d /dev/vfio ]; then
+	exit 1
+fi
+# basic sanity check: is the control device available?
+if [ ! -r /dev/vfio/vfio ] || [ ! -w /dev/vfio/vfio ]; then
+	exit 2
+fi
 
+# ok, now we can check the real device access.
+# 1. find all the <vfid>s allocated to the container.
+#  Provided by the sriov device plugin, this will be a comma separated PCI address list
+PCIDEVS=$( env | grep PCIDEVICE | cut -d\= -f2 )
+# hold on just a sec: did we got any allocated devices at all?
+if [ -z "${PCIDEVS}" ]; then
+	exit 4
+fi
+
+# 2. figure out all /dev/vfio/<vfid> allocated devices
+for PCIDEV in $( echo $PCIDEVS | tr ',' '\n' ); do
+	# learn the vfio ID, which is the iommu_group (see https://docs.kernel.org/driver-api/vfio.html)
+	# we can learn this value from the PCI device data exposed by the kernel on sysfs
+	VFIOID=$( basename $( readlink /sys/bus/pci/devices/$PCIDEV/iommu_group ) );
+
+	# 3. crash hard if any of the allocated devices are not are accessible within the container (R_ACCESS+W_ACCESS)
+	if [ ! -r /dev/vfio/$VFIOID ] || [ ! -w /dev/vfio/$VFIOID ]; then
+		exit 8
+	fi
+done
+
+# 4. sleep forever otherwise
 sleep inf
